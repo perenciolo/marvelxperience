590789f5fffc6596c666c08dd43dde206df41958
Gustavo Perenciolo <gustavo@blackowl.com.br>
2017-07-23 23:52:12 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 };
 
 // Marvel API Keys
-export const publicKey = '94ac9928f9a03f3a3663a251fd17d957';
-export const privateKey = '13c941c455fd986fbe3cf062e5377fdfe58ec9c5';
+export const publicKey = ADD YOUR MARVEL PUBLIC KEY HERE;
+export const privateKey = ADD YOUR MARVEL PRIVATE KEY HERE;
 
 // Marvel Endpoint
 export const endpoint = 'https://gateway.marvel.com:443/v1/public';
